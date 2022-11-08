4270d649f1c2cc42441e2c7587ea8311bc6a19fa
Berdan <berdan.akyurek@ug.bilkent.edu.tr>
2022-11-08 14:29:36 +0300

    disable web mode auto quote

--- init.el
+++ init.el
@@ -1368,6 +1368,7 @@
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-indent-style 2)
+  (setq web-mode-enable-auto-quoting nil)
   )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
