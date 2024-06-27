fb73a310a326d711ecbc92bf9197c44b631aaacc
Berdan Akyurek <berdan@teus.com.tr>
2024-06-27 16:16:31 +0300

    gitignore

---
+++ elisp/custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-noise-macro-names '("constexpr"))
+ '(lsp-javascript-update-imports-on-file-move-enabled "never")
+ '(lsp-typescript-update-imports-on-file-move-enabled "never")
+ '(package-selected-packages '(emacsconf-update dashboard))
+ '(package-vc-selected-packages
+   '((vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ahs-face ((t (:background "#525765" :foreground "#AEAFB1" :weight extra-bold))))
+ '(ahs-face-unfocused ((t (:background "#525765" :foreground "#AEAFB1" :weight extra-bold))))
+ '(ahs-plugin-default-face ((t (:background "#525765" :foreground "#AEAFB1" :weight extra-bold))))
+ '(ahs-plugin-default-face-unfocused ((t (:background "#525765" :foreground "#AEAFB1" :weight extra-bold)))))
